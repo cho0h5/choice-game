9c4478db9d95e625b4511bc84a65eb02b051939b
Youngho Cho <dominoyh5@gmail.com>
2024-11-16 14:47:24 +0900

    impl: difficulty_of_choices and difficulty_delta but addend can be negative integer

--- main.py
+++ main.py
@@ -5,15 +5,32 @@ def generate_initial_player_state():
     return player_state
 
 def generate_choices(player_state, difficulty_of_choices, difficulty_delta, multiplier_range):
+    # 곱하기 연산자의 오른쪽 피연산자 생성
     multiplier = random.randint(1, multiplier_range)
+    # 곱하기 연산 했을 때 정답 계산
     answer = player_state * multiplier
+    # 정답에서 역산하여 더하기 연산자의 오른쪽 피연산자 계산
     addend = answer - player_state
-    return (('*', multiplier), '+', addend)
+
+    # difficulty_of_choices에 변동 추가
+    delta = random.randint(-difficulty_delta, difficulty_delta)
+    adjusted_difficulty = difficulty_of_choices + delta
+    print(adjusted_difficulty)
+
+    # 절반의 확률로 addend에서 difficulty_of_choices를 더하거나 뺌
+    if random.choice([True, False]):
+        addend += adjusted_difficulty
+    else:
+        addend -= adjusted_difficulty
+
+    return (('*', multiplier), ('+', addend))
 
 if __name__ == "__main__":
+    DIFFICULTY_OF_CHOICES = 10
+    DIFFICULTY_DELTA = 1
+    MULTIPLIER_RANGE = 10
+
     player_state = generate_initial_player_state()
-    difficulty_of_choices = 10
-    difficulty_delta = 0
-    multiplier_range = 10
-    choices = generate_choices(player_state, difficulty_of_choices, difficulty_delta, multiplier_range)
+
+    choices = generate_choices(player_state, DIFFICULTY_OF_CHOICES, DIFFICULTY_DELTA, MULTIPLIER_RANGE)
     print(player_state, choices)
