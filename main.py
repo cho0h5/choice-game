903c01133c44c45505e4efc67dc0f85977a9cf77
Youngho Cho <dominoyh5@gmail.com>
2024-11-19 21:17:46 +0900

    remove: difficulty delta

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def generate_initial_player_state():
     player_state = random.randint(1, 9)
     return player_state
 
-def generate_choices(player_state, difficulty_of_choices, difficulty_delta, multiplier_range):
+def generate_choices(player_state, difficulty_of_choices, multiplier_range):
     # 곱하기 연산자의 오른쪽 피연산자 생성
     multiplier = random.randint(1, multiplier_range)
     # 곱하기 연산 했을 때 정답 계산
@@ -14,12 +14,8 @@ def generate_choices(player_state, difficulty_of_choices, difficulty_delta, mult
     # 정답에서 역산하여 더하기 연산자의 오른쪽 피연산자 계산
     addend = answer - player_state
 
-    # difficulty_of_choices에 변동 추가
-    delta = random.randint(-difficulty_delta, difficulty_delta)
-    adjusted_difficulty = difficulty_of_choices + delta
-
     # 절반의 확률로 addend에서 difficulty_of_choices를 더하거나 뺌
-    addend += adjusted_difficulty if random.choice([True, False]) else -adjusted_difficulty
+    addend += difficulty_of_choices if random.choice([True, False]) else -difficulty_of_choices
 
     # 선택지의 순서를 섞음
     if random.choice([True, False]):
@@ -41,7 +37,6 @@ def print_choices(choices):
 
 if __name__ == "__main__":
     DIFFICULTY_OF_CHOICES = 5
-    DIFFICULTY_DELTA = 1
     MULTIPLIER_RANGE = 10
 
     print(3)
@@ -54,7 +49,7 @@ if __name__ == "__main__":
 
     while True:
         player_state = generate_initial_player_state()
-        choices = generate_choices(player_state, DIFFICULTY_OF_CHOICES, DIFFICULTY_DELTA, MULTIPLIER_RANGE)
+        choices = generate_choices(player_state, DIFFICULTY_OF_CHOICES, MULTIPLIER_RANGE)
 
         print("player_state:", player_state, sep='\t')
         print_choices(choices)
